7d312a5150e54a39995798eb0bf94a0da89d6dd1
juanchaves <chavesjuanm@gmail.com>
2017-03-03 12:12:01 -0800

    additional set-ups and restructuring

--- app.js
+++ app.js
@@ -11,16 +11,19 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view options', { layout: 'templates/layout' });
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('images', express.static(path.join(__dirname, 'public/images')));
 
 app.use('/', index);
 app.use('/users', users);
